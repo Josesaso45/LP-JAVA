1bc2a06f4ddc8386c85b886a1d5bb2ccb14684c0
Jose Montero <josemontero2415@gmail.com>
2026-04-05 13:59:38 -0500

    Cambio de estructura de carpetas y cursos de JAVA

--- cobranzas/java/com/agv/cobranzas/repository/PlanillaLetrasRepository.java
+++
@@ -1,9 +0,0 @@
-package com.agv.cobranzas.repository;
-
-import com.agv.cobranzas.model.PlanillaLetras;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PlanillaLetrasRepository extends JpaRepository<PlanillaLetras, Long> {
-}
--- mt/src/main/java/com/mitienda/mt/repository/TipoClienteRepository.java
+++
@@ -1,7 +0,0 @@
-package com.mitienda.mt.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.mitienda.mt.model.TipoCliente;
-
-public interface TipoClienteRepository extends JpaRepository<TipoCliente, Integer> {
-}
